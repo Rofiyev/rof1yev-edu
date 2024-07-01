35726bbbd3a1224944ed909df513ba8de769ca1b
Rofiyev Dilshod <97677072+Rofiyev@users.noreply.github.com>
2024-07-01 09:55:17 +0500

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,9 @@ import theme from "@/theme";
 export default function Document(): JSX.Element {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="TWhx17k6Q6zi64nwcE1mT0QBcnOPl-ARF1TRx_lWQVA" />
+      </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
